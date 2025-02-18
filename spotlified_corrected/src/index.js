17473b1c6a7288dedbd17a0095d1b989488a8584
RobinChappalley <robinchappalley@gmail.com>
2025-02-18 16:39:40 +0100

    add custmo elements and adapt css

--- spotlified_corrected/src/index.js
+++ spotlified_corrected/src/index.js
@@ -1,4 +1,21 @@
+class ArtistCover extends HTMLElement {
+  static observedAttributes = ["image_url", "name"];
+  connectedCallback() {
+    this.render();
+  }
+  attributeChangedCallback() {
+    this.render();
+  }
+  render() {
+    this.innerHTML = `
+    <img src="${this.getAttribute("cover")}"/>
+    <div class="artist-list-item-title">${this.getAttribute("name")}</div>
+    `;
+  }
+}
+customElements.define("artist-cover", ArtistCover);
 console.log("It works !");
+
 const URLS = {
   artists: `https://webmob-ui-22-spotlified.herokuapp.com/api/artists`,
 };
@@ -13,16 +30,11 @@ const displayArtists = async (artistTable) => {
   const sectionArtist = document.querySelector(".artist-list");
   sectionArtist.innerHTML = "";
   artistTable.forEach((artist) => {
-    const link = document.createElement("a");
-    link.href = "#";
-    const img = document.createElement("img");
-    img.src = artist.image_url;
-    link.appendChild(img);
-    const div = document.createElement("div");
-    div.innerHTML = artist.name;
-    div.classList.add("artist-list-item-title");
-    link.appendChild(div);
-    sectionArtist.appendChild(link);
+    console.log(artist);
+    const artistElement = document.createElement("artist-cover");
+    artistElement.setAttribute("cover", artist.image_url);
+    artistElement.setAttribute("name", artist.name);
+    sectionArtist.appendChild(artistElement);
   });
 };
 
