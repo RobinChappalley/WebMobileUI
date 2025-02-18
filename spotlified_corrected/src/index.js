2eaa087a325aeba5922e83811e0b5d7ac0846e16
RobinChappalley <robinchappalley@gmail.com>
2025-02-18 14:55:53 +0100

    add function to display artist

--- spotlified_corrected/src/index.js
+++ spotlified_corrected/src/index.js
@@ -1,1 +1,29 @@
-console.log('It works !')
+console.log("It works !");
+const URLS = {
+  artists: `https://webmob-ui-22-spotlified.herokuapp.com/api/artists`,
+};
+
+const getArtists = async () => {
+  const response = await fetch(URLS.artists);
+  const artists = await response.json();
+  return artists;
+};
+
+const displayArtists = async (artistTable) => {
+  const sectionArtist = document.querySelector(".artist-list");
+  sectionArtist.innerHTML = "";
+  artistTable.forEach((artist) => {
+    const link = document.createElement("a");
+    link.href = "#";
+    const img = document.createElement("img");
+    img.src = artist.image_url;
+    link.appendChild(img);
+    const div = document.createElement("div");
+    div.innerHTML = artist.name;
+    div.classList.add("artist-list-item-title");
+    link.appendChild(div);
+    sectionArtist.appendChild(link);
+  });
+};
+
+displayArtists(await getArtists());
