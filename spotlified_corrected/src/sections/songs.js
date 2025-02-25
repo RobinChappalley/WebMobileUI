3edff1812e36e51d20073ddaf4d0dce1c1b5bb78
RobinChappalley <robinchappalley@gmail.com>
2025-02-25 15:16:36 +0100

    add infos on the  player

--- spotlified_corrected/src/sections/songs.js
+++ spotlified_corrected/src/sections/songs.js
@@ -1,38 +1,55 @@
-import { loadSongs } from '../api.js'
+import { loadSongs } from "../api.js";
 
 // Récupérer le tag contenant la liste des chansons et le titre de la section
-const songList = document.querySelector('.list')
-const titreList = document.querySelector('#list-section h4')
+const songList = document.querySelector(".list");
+const titreList = document.querySelector("#list-section h4");
 
 // S'occupe d'afficher les chansons d'un artiste, selon son ID.
 // Pour cela, on va utiliser loadSongs du fichiers api.js qui lui sait nous retourner
 // un tableau de chanson, selon l'id d'un artiste
 const displayArtistSongs = async (id) => {
   // Récupérer la liste des chansons depuis l'api
-  const songs = await loadSongs(id)
+  const songs = await loadSongs(id);
 
   // Titre à jour
-  titreList.innerHTML = `Artistes > ${songs[0].artist.name}`
+  titreList.innerHTML = `Artistes > ${songs[0].artist.name}`;
 
   // Vider la liste
-  songList.innerHTML = ''
+  songList.innerHTML = "";
 
-// Itérer le tableau d'artistes reçus et créer les éléments correspondants
+  // Itérer le tableau d'artistes reçus et créer les éléments correspondants
   songs.forEach((song) => {
     // Créer l'élément
-    const songItem = document.createElement('song-item')
+    const songItem = document.createElement("song-item");
 
-    songItem.setAttribute('title', song.title)
-    songItem.setAttribute('favorite', false) // ou true, pour plus tard
+    songItem.setAttribute("title", song.title);
+    songItem.setAttribute("favorite", false); // ou true, pour plus tard
 
     // Insérer dans la liste
-    songList.appendChild(songItem)
+    songList.appendChild(songItem);
+    songItem.addEventListener("click", () => {
+      playSong(song, songs);
+      displaySongInfos(song);
+    });
+  });
+};
 
-    const audioTag = document.querySelector('audio')
-    audioTag.src = song.audio_url
-    audioTag.play() // Pour démarrer la musique automatiquement
-    console.log(song)
-  })
-}
+// Fonction pour jouer une musique
 
-export { displayArtistSongs }
+const playSong = async (song, songs) => {
+  const audioTag = document.querySelector("audio");
+  audioTag.src = song.audio_url;
+  audioTag.play(); // Pour démarrer la musique automatiquement
+  // console.log(song);
+  console.table(songs);
+};
+
+const displaySongInfos = async (song) => {
+  const coverImg = document.querySelector('#player-thumbnail-image')
+  coverImg.src = song.artist.image_url;
+  const title = document.querySelector('#player-infos-song-title');
+  title.innerHTML = song.title;
+  const artist = document.querySelector('#player-infos-artist-name');
+  artist.innerHTML = song.artist.name;
+};
+export { displayArtistSongs };
