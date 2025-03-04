c3105c204d999adae8158a88669d2ba295e7b3ac
RobinChappalley <robinchappalley@gmail.com>
2025-03-04 15:54:06 +0100

    Add query for search with encoding and decoding

--- spotlified_corrected/src/api.js
+++ spotlified_corrected/src/api.js
@@ -1,9 +1,16 @@
-const BASE_URL = 'https://webmob-ui-22-spotlified.herokuapp.com'
+const BASE_URL = "https://webmob-ui-22-spotlified.herokuapp.com";
 
-const loadJson = (url) => fetch(url).then((response) => response.json())
+const loadJson = (url) => fetch(url).then((response) => response.json());
 
-const loadArtists = () => loadJson(`${BASE_URL}/api/artists`)
+const loadArtists = () => loadJson(`${BASE_URL}/api/artists`);
 
-const loadSongs = (id) => loadJson(`${BASE_URL}/api/artists/${id}/songs`)
+const loadSongs = (id) => loadJson(`${BASE_URL}/api/artists/${id}/songs`);
 
-export { loadArtists, loadSongs }
+const search = (query) =>
+  loadJson(
+    `${BASE_URL}/api/songs/search/${encodeURIComponent(
+      decodeURIComponent(query)
+    )}`
+  );
+export { search };
+export { loadArtists, loadSongs };
